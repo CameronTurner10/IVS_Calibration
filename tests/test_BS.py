9fdaa170afc09c0ca22299d29e97f544eed8df4f
Brandon Rumball <brandonrumball2003@gmail.com>
2025-12-02 19:19:41 +0000

    Made changes to a staged version then commited the outdated one, fixed now

--- tests/test_BS.py
+++ tests/test_BS.py
@@ -1,13 +1,11 @@
-# oneway BS
-# Call-Put Parity
+# oneway BS + Call-Put Parity
 
 
 import numpy as np
 import pandas as pd
-from math import exp
 from src.utils.black_scholes import bs_call, bs_put
 
-def test_oneway_BS():
+def test_oneway_and_parity_BS():
     excel = pd.ExcelFile("tests/data/Surfaces.xlsx")
     
     for sheet in excel.sheet_names:
@@ -22,8 +20,11 @@ def test_oneway_BS():
             put_expected = row["Put Price"]
             r = row["Discount Rate"]
 
+            # oneway test
             our_call_model = bs_call(F,K,T,sigma,r) 
             our_put_model = bs_put(F,K,T,sigma,r)
+
+            # call-put parity test
             lhs = our_call_model - our_put_model
             rhs = np.exp(-r * T) * (F - K)
 
@@ -31,5 +32,5 @@ def test_oneway_BS():
             assert np.isclose(our_put_model, put_expected, atol=1e-5), f">>> PUT model does NOT return expected value in {sheet} <<<"
             assert np.isclose(lhs, rhs, atol=1e-5), f">>> Put-Call Parity failed for {sheet} with params F={F}, K={K}, T={T}, r={r} <<<"
         
-            # for the atol, keep it at a reasonalbly small but not too small as excel and computer noise is likely getting involved at 
-            # approx 1e-08 which will make all tests fail when they are actually good.
+            # for the atol, keep it at a reasonalbly small but not too small as excel and computer noise could get involved at 
+            # approx 1e-08 which will make all tests fail when they are actually good. 
