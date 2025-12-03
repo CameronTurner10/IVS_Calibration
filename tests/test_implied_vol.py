c12f5be4cafe3754b291dc2376231cb99e8e3e4c
CameronTurner10 <C.Turner2-23@student.lboro.ac.uk>
2025-12-03 18:16:40 +0000

    Cleaned up

--- tests/test_implied_vol.py
+++ tests/test_implied_vol.py
@@ -1,23 +1,8 @@
 # one-way IV test
-
-
 import numpy as np
 import pandas as pd
 from src.utils.black_scholes import bs_call, bs_put
-from scipy.optimize import brentq
-
-
-def implied_vol(F,K,T,r,market_price,option_type="call"):
-    
-    def f_sigma(sigma,):
-        if option_type=="call":
-            return bs_call(F,K,T,sigma,r)-market_price
-        else:
-            return bs_put(F,K,T,sigma,r)-market_price
-    
-    return brentq(f_sigma,1e-10,5)
-#For sigma on the interval [a,b] brent solver finds f(sigma)=0
-# Here a = 1e-8 and b = 5  (f(a)*f(b)<0 else -> exit)
+from src.utils.root_finder import implied_vol
 
 def test_implied_vol():
     excel = pd.ExcelFile("tests/data/Surfaces.xlsx")
