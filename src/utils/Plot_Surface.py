f1283b0567317998f18d466c26d9a51d4a9df241
CameronTurner10 <C.Turner2-23@student.lboro.ac.uk>
2026-02-11 17:56:13 +0000

    Single and Multi Slice plotting functions for the SVI

---
+++ src/utils/Plot_Surface.py
@@ -0,0 +1,7 @@
+# SVI Surface Plot
+from svi.optimisation.SVI_SliceFit import fit_svi_slice
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from src.utils.Plot_Single_Slice import svi_single_slice
+
