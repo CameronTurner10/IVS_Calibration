ca81abd91991a48ed9905cbc470db778d4f944d3
Brandon Rumball <brandonrumball2003@gmail.com>
2026-03-08 09:30:01 +0000

    Refactor SVI calibration to sequential slice-fitting with SHGO/trust-constr and add Calendar constraint going backwards in time. Added TODO for Jack's Butterfly arb.

--- src/svi/optimisation/local_optimizers.py
+++ src/svi/optimisation/local_optimizers.py
@@ -1,8 +1,8 @@
 # SVI fitting using scipy - local optimisation methods
 # Goal: find the 5 params (a, b, rho, m, sigma) that best match market data
 import numpy as np
+import pandas as pd
 from scipy.optimize import minimize
-from src.svi.implementation.svi_model import SVI
 
 # Bounds for the 5 SVI parameters - shared across all local methods
 SVI_BOUNDS = [
@@ -16,31 +16,11 @@ SVI_BOUNDS = [
 def total_variance(k, a, b, rho, m, sigma):
     return a + b * (rho * (k - m) + np.sqrt((k - m)**2 + sigma**2))
 
-def svi_objective(parameters, k, w_market): #Objective function shared by all local methods - minimises sum of squared errors.
+def svi_objective(parameters, k, w_market): 
+    # Objective function shared by all local methods - minimises sum of squared errors.
     a, b, rho, m, sigma = parameters
     w_model = total_variance(k, a, b, rho, m, sigma)
     return np.sum((w_model - w_market) ** 2)
-
-def svi_full_objective(parameters, k_list, w_market_list): #Objective function shared by all local methods - minimises sum of squared errors.
-    total_sum=0
-    for i in range(len(k_list)):
-        slice_params = parameters[i*5:(i+1)*5] 
-        w_model = total_variance(k_list[i], *slice_params)
-        total_sum += np.sum((w_model - w_market_list[i])**2)
-    return total_sum
-
-def calendar_constraints(parameters,k_list):
-    constraints=[]
-    for i in range(1,len(k_list)):     #parameters[i*5:(i+1)*5] -> i=0 sliceT[0] parameters [0:5] = [a0,b0,rho0,m0,sigma0] if i=1 parameters [5,10]=[a1,b1,rho1,m1,sigma1]
-        w_current=total_variance(k_list[i],*parameters[i*5:(i+1)*5]) #k_list[i] ->k's for T[i]
-        w_previous=total_variance(k_list[i-1],*parameters[(i-1)*5:i*5]) #k_list[i-1] ->k's for T[i-1]
-
-        # interpolate previous slice to current ks -> Need to do this so w_current and w_previous are evaluated at same k[i]
-        w_prev_at_curr_ks = np.interp(k_list[i], k_list[i-1], w_previous)
-
-        constraints.append(w_current - w_prev_at_curr_ks)
-        #constraints.append(w_current - w_previous)
-    return np.concatenate(constraints) #returns a single flat array of all contsraints
 
 #below are the different local optimisers
 
@@ -55,57 +35,11 @@ def fit_svi_slsqp(strikes, market_vols, T, forward): #Sequential Least Squares P
         svi_objective,
         x0,
         args=(k_values, w_market),
-        method='SLSQP',
+        method="SLSQP",
         bounds=SVI_BOUNDS
     )
 
-    return dict(zip(['a', 'b', 'rho', 'm', 'sigma'], result.x))
-
-def fit_svi_multi_slsqp(sheet_name,filepath="tests/data/Surfaces.xlsx"): #Sequential Least Squares Programming
-
-    df = pd.read_excel(filepath, sheet_name=sheet_name)
-    expiries = sorted(df["Year Fraction"].unique())
-    
-    x0=[]
-    k_list=[] 
-    w_market_list=[]
-
-    for T in expiries:
-        strikes, market_vols, forward, k_values, w_market = get_slice_from_data(T, sheet_name)
-        k_list.append(k_values) 
-        w_market_list.append(w_market)
-
-    #print(k_list)
-    #print("length k_list")
-    #print(len(k_list))
-    #print(len(expiries))
-
-    for w_market in w_market_list:
-        atm_var = np.mean(w_market)
-        x0.extend([atm_var, 0.1, 0.0, 0.0, 0.1] ) # [a, b, rho, m, sigma] initial guess 
-
-    number_of_slices = len(expiries)
-    bounds = SVI_BOUNDS * number_of_slices # bounds for each slice 
-
-    result = minimize(
-        svi_full_objective,
-        x0,
-        args=(k_list, w_market_list),
-        method='SLSQP',
-        bounds=bounds,
-                              #>=0         anonymous function            constraints
-        constraints=[{'type':'ineq', 'fun': lambda p:          calendar_constraints(p, k_list)}] 
-    )
-    fitted_slices = []
-    for i in range(len(expiries)):
-        slice_params = result.x[i*5:(i+1)*5]
-        fitted_slices.append(dict(zip(['a','b','rho','m','sigma'], slice_params))) #fitted parameters for each slice
-
-    #print(len(fitted_slices))
-    #print(len(x0))
-    #print(len(bounds))
-
-    return fitted_slices
+    return dict(zip(["a", "b", "rho", "m", "sigma"], result.x))
 
 
 def fit_svi_trust_region(strikes, market_vols, T, forward): #Trust Region Constrained optimisation
@@ -137,7 +71,7 @@ def fit_svi_cobyqa(strikes, market_vols, T, forward): #Constrained Optimisation 
         svi_objective,
         x0,
         args=(k_values, w_market),
-        method='COBYQA',
+        method="COBYQA",
         bounds=SVI_BOUNDS
     )
 
